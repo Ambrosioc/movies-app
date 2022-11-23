406284b5019a85c0e6d728f73f393ac8b3880be3
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-11-23 19:55:57 +0100

    add security in API_URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
 // c17d686d
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c17d686d";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=c17d686d";
 
 function App() {
   const [movies, setMovies] = useState([]);
