b43bebf4f5ce71a326013d85d16054db8f27d5c7
Ambrosie Cazimira <a.cazimira@gmail.com>
2023-07-30 13:57:30 +0200

    add a new experiace user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import Search from "../components/Search";
 import MovieCard from "../components/MovieCard";
+import { useFetch } from "../utils/hooks";
+
+const URL_2 = "https://www.omdbapi.com/?i=tt3896198&apikey=c17d686d";
 
 export default function Home({ movies, setMovies }) {
 	const [searchValue, setSearchValue] = useState("");
+
+	const { data, error, isLoading } = useFetch(URL_2);
+	console.log(data, movies);
+
+	if (isLoading) {
+		return <h2>Loading...</h2>;
+	}
 
 	return (
 		<div className="app">
@@ -13,6 +23,25 @@ export default function Home({ movies, setMovies }) {
 				setSearchValue={setSearchValue}
 				setMovies={setMovies}
 			/>
+			{error && <h2>{error}</h2>}
+			{searchValue ? (
+				<h2>Search results for: {searchValue}</h2>
+			) : (
+				<>
+					<h2>Popular movies</h2>
+					<br />
+					<div className="container">
+						{data ? (
+							data.map((movie, index) => (
+								<MovieCard key={index} movie={movie} />
+							))
+						) : (
+							<h2>No movies founds</h2>
+						)}
+					</div>
+				</>
+			)}
+
 			<div className="container">
 				{movies?.length > 0 ? (
 					movies.map((movie, index) => <MovieCard key={index} movie={movie} />)
---
+++ src/utils/hooks/index.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(url) {
+	const [data, setDada] = useState(null);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const dataArray = [];
+
+	if (!Array.isArray(url)) {
+		dataArray.push(url);
+	} else {
+		dataArray.push(...url);
+	}
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await Promise.all(
+					dataArray.map((url) => fetch(url).then((res) => res.json()))
+				);
+				setDada(response);
+				setIsLoading(false);
+			} catch (error) {
+				setError(error.message);
+				setIsLoading(false);
+			}
+		};
+		fetchData();
+	}, []);
+
+	return { data, error, isLoading };
+}
